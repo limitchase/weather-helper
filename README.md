fd44f349f1c7359e090e78bada0487f2bab75434
Chase Lim <limitchase@gmail.com>
2023-04-07 05:00:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Weather Helper
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[Live Demo](https://cl-weatherhelper.netlify.app/)
+
+![preview img](./public/weatherhelper-preview.png)
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#0369a1",
+        "weather-secondary": "#0c4a6e",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
